728ce7e4e02e92aef97355c7b45f43ddd3479894
klaercio <klaerciosilva@gmail.com>
2022-04-21 22:46:51 -0300

    Criado um erro em nodejs

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.green('Vamos Começar!'));
+/* console.log(chalk.green('Vamos Começar!'));
 
 const paragragfo = 'Texto retornado por uma função';
+
+console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
 
 function texto(string) {    
     return string;
 }
 
-console.log(texto(paragragfo));
+console.log(texto(paragragfo)); */
+
+function tratarErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    fs.readFile(caminhoDoArquivo, 'utf-8', (err, data) => {
+        if (err) 
+            return tratarErro(err);
+        console.log(chalk.green(data));    
+    })
+}
+
+pegaArquivo('./arquivos/texto1.md');
