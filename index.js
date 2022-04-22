05e07d572d72b43b515a4e2cf078ec570c702c2a
klaercio <klaerciosilva@gmail.com>
2022-04-21 22:03:32 -0300

    incluindo a primeira biblioteca

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Vamos Começar!'));
+
+const paragragfo = 'Texto retornado por uma função';
+
+function texto(string) {    
+    return string;
+}
+
+console.log(texto(paragragfo));
