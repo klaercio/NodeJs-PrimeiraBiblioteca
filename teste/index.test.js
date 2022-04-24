23bbc870276c7df78566cb2ec6ae64b31d4538c3
klaercio <klaerciosilva@gmail.com>
2022-04-23 23:28:03 -0300

    refatorando os codigos do arquivo de teste

--- teste/index.test.js
+++ teste/index.test.js
@@ -1,5 +1,18 @@
 import pegaArquivo from '../index.js';
 
-test('deve ser uma função', () => {
-    expect(typeof pegaArquivo).toBe('function');
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('pegaArquvo::', ()=> {
+    it('deve ser uma função', ()=>{
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('deve retornar array com resultados', async ()=> {
+        const resultado = await pegaArquivo('./texto1.md');
+        console.log(resultado);
+        expect(resultado).toEqual(arrayResult);
+    })
 })
