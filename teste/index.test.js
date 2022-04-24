27fadff8b002a9266d8ab0d9839a8f7de33f0564
klaercio <klaerciosilva@gmail.com>
2022-04-23 23:45:13 -0300

    adicionando mais casos de teste

--- teste/index.test.js
+++ teste/index.test.js
@@ -11,8 +11,12 @@ describe('pegaArquvo::', ()=> {
         expect(typeof pegaArquivo).toBe('function');
     })
     it('deve retornar array com resultados', async ()=> {
-        const resultado = await pegaArquivo('./texto1.md');
+        const resultado = await pegaArquivo('./teste/arquivos/texto1.md');
         console.log(resultado);
         expect(resultado).toEqual(arrayResult);
+    })
+    it('deve retornar mensagem "não há links"', async () => {
+        const resultado = await pegaArquivo('./teste/arquivos/texto1_semlinks.md');
+        expect(resultado).toBe('Arquivo não contem links');
     })
 })
